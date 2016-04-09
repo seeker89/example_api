b7a0db83460046c5edb648194935005c84d30944
Mikolaj Pawlikowski <mikolaj@pawlikowski.pl>
2016-04-09 13:48:00 +0100

    setup a basic structure with good logging

---
+++ server/libs/logger.js
@@ -0,0 +1,26 @@
+var winston = require('winston');
+
+var consoleLogger = new winston.transports.Console({
+    level: 'debug',
+    timestamp: function() {
+        return new Date().toString();
+    },
+    colorize: true,
+    prettyPrint: true,
+});
+
+var logger = new winston.Logger({
+    transports: [
+        consoleLogger
+    ],
+    exitOnError: false
+});
+logger.stream = {
+    write: function(message, encoding){
+        logger.debug(message);
+    }
+};
+
+logger.info('Logger initialized at ' + new Date());
+
+module.exports = logger;
