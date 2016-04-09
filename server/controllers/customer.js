dccb693412025f1fceb271fba8f2fc1f75469b23
Mikolaj Pawlikowski <mikolaj@pawlikowski.pl>
2016-04-09 19:10:54 +0100

    implemented the Customer endpoint

--- server/controllers/customer.js
+++ server/controllers/customer.js
@@ -1,15 +1,36 @@
 'use strict';
 
-var url = require('url');
+var logger = require("../libs/logger");
+var handler = require("./errors");
 
-
-var Customer = require('./CustomerService');
+var CustomerManager = require('../models/customer_memory');
 
 
 module.exports.createCustomer = function createCustomer (req, res, next) {
-  Customer.createCustomer(req.swagger.params, res, next);
+
+    var name = req.swagger.params.body.value.name;
+
+    CustomerManager.create({
+        name: name
+    }).then(function(customer){
+        res.send(customer.toObject());
+    }).catch(function(err){
+        handler.send(err);
+    });
 };
 
 module.exports.getCustomer = function getCustomer (req, res, next) {
-  Customer.getCustomer(req.swagger.params, res, next);
+
+    var id = req.swagger.params.customerId.value;
+    
+    CustomerManager.findById(id)
+    .then(function(customer){
+        if (!customer){
+            handler.notFound(res);
+        } else {
+            res.send(customer.toObject());
+        }
+    }).catch(function(err){
+        handler.send(err);
+    });
 };
