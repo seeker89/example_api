783acc659af820d42337c12c80e923071cdae1cf
Mikolaj Pawlikowski <mikolaj@pawlikowski.pl>
2016-04-09 14:11:49 +0100

    Bootstrapped tests

---
+++ server/test/customer_test.js
@@ -0,0 +1,33 @@
+var assert = require('chai').assert;
+var expect = require('chai').expect;
+var Promise = require("bluebird");
+
+var Customer = require("../models/customer");
+
+
+describe('Customer', function() {
+
+    describe('#get & set', function() {
+
+        it('should get and set things', function() {
+
+            var storage = {};
+
+            var usr = new Customer(storage);
+            usr.set("name", "Donald");
+
+            expect(storage).to.have.property("name");
+
+            expect(usr.get("name")).to.be.equal("Donald");
+            expect(storage.name).to.be.equal("Donald");
+
+            usr.set("name", "Duck");
+
+            expect(usr.get("name")).to.be.equal("Duck");
+            expect(storage.name).to.be.equal("Duck");
+
+        });
+
+    });
+
+});
