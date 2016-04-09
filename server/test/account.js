1e29ac15c0cbb94fe35aa9175c919945f0ba1542
Mikolaj Pawlikowski <mikolaj@pawlikowski.pl>
2016-04-09 15:17:08 +0100

    Added Account model, very similar to Customer

---
+++ server/test/account.js
@@ -0,0 +1,112 @@
+var assert = require('chai').assert;
+var expect = require('chai').expect;
+var Promise = require("bluebird");
+
+var Account = require("../models/Account");
+
+
+describe('Account', function() {
+
+    describe('#constructor', function() {
+
+        it('should not accept no storage', function() {
+
+            expect(function(){
+                new Account(undefined);
+            }).to.throw(RangeError);
+
+        });
+
+    });
+
+    describe('#get & set', function() {
+
+        it('should get and set things', function() {
+
+            var storage = {};
+
+            var usr = new Account(storage);
+            usr.set("name", "Current Account");
+
+            expect(storage).to.have.property("name");
+
+            expect(usr.get("name")).to.be.equal("Current Account");
+            expect(storage.name).to.be.equal("Current Account");
+
+            usr.set("name", "Savings Account");
+
+            expect(usr.get("name")).to.be.equal("Savings Account");
+            expect(storage.name).to.be.equal("Savings Account");
+
+        });
+
+    });
+
+
+    describe('#save', function() {
+
+        it('should save things', function(done) {
+
+            var usr;
+
+            var storage = {};
+            storage.save = function() {
+                return Promise.resolve(usr);
+            };
+
+            usr = new Account(storage);
+
+            usr.save()
+                .then(function(u){
+                    assert(u === usr)
+                    done();
+                })
+                .catch(function(err){
+                    done(err);
+                });
+        });
+
+    });
+
+
+    describe('#dot notation', function() {
+
+        it('should work like a charm', function() {
+
+            var values = {
+                name: "Current Account",
+                number: "curr-12346",
+                ownerID: "1"
+            }
+
+            var storage = {};
+
+            var usr = new Account(storage);
+            usr.set("name", values.name);
+            usr.set("number", values.number);
+            usr.set("ownerID", values.ownerID);
+
+            expect(usr.name).to.be.equal(values.name);
+            expect(usr.number).to.be.equal(values.number);
+            expect(usr.ownerID).to.be.equal(values.ownerID);
+
+            expect(usr.doesntexitst).to.be.equal(undefined);
+        });
+
+    });
+
+   describe('#toObject', function() {
+
+        it('should work like a charm', function() {
+            var storage = {};
+            var usr = new Account(storage);
+            expect(usr.toObject()).to.be.deep.eql(storage);
+        });
+
+    });
+
+
+
+
+
+});
