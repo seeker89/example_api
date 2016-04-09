cc3a11dbc5c2a37963829c0da7a467a6f621816d
Mikolaj Pawlikowski <mikolaj@pawlikowski.pl>
2016-04-09 14:41:39 +0100

    added customer_memory in-memory store for customers

---
+++ server/models/customer_memory.js
@@ -0,0 +1,57 @@
+var Promise = require("bluebird");
+var Moment = require("moment");
+var _ = require("lodash");
+
+var Customer = require("../models/customer");
+
+var __store;
+
+/*
+
+    This is a
+
+
+ */
+
+module.exports = {
+
+    create: function(data){
+
+    },
+
+    findById: function(id){
+
+    },
+
+
+    /*
+        For testing, so that we can "reset" the contents of the store
+     */
+    __store: __store
+};
+
+
+
+/*
+
+    Prefilling the store with some data to begin with
+
+ */
+store = [
+    {
+        "id": 1,
+        "name": "Jane Woods"
+    },
+    {
+        "id": 2,
+        "name": "Michael Li"
+    },
+    {
+        "id": 3,
+        "name": "Heidi Hasselbach"
+    },
+    {
+        "id": 4,
+        "name": "Rahul Pour"
+    }
+];
---
+++ server/test/customer_memory_test.js
@@ -0,0 +1,29 @@
+var assert = require('chai').assert;
+var expect = require('chai').expect;
+var Promise = require("bluebird");
+
+var CustomerMemory = require("../models/customer_memory");
+
+
+describe('Customer', function() {
+
+    describe('#find', function() {
+
+        it('should find one of the predefined customers', function(done) {
+
+            CustomerMemory.findById(3)
+                
+                .then(function(customer){
+                    expect(customer.name).to.be.equal("Heidi Hasselbach");
+                    expect(customer.id).to.be.equal(3);
+                    done();
+                })
+                .catch(function(err){
+                    done(err);
+                });
+
+        });
+
+    });
+
+});
