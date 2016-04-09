d8e445f09b69a1811cf86811414ff818268501cb
Mikolaj Pawlikowski <mikolaj@pawlikowski.pl>
2016-04-09 16:17:38 +0100

    implemented transaction model

---
+++ server/models/transaction.js
@@ -0,0 +1,67 @@
+var Promise = require("bluebird");
+
+var logger = require("../libs/logger");
+
+/*
+
+    The Transaction model.
+
+    Since we're going to have at least two swappable backends (in-memory + db),
+    this model will abstract the user manipulations by wrapping the actual data
+    in a higher level, nice API.
+
+
+    params:
+        - storage - this will be assumed to be a promised-based storage (save),
+                    accepting dot notation for setting and getting properties.
+
+
+
+ */
+function Transaction(storage) {
+
+    if (!storage){
+        throw new RangeError("Cannot instantiate Transaction with no underlying storage");
+    }
+
+    // save to the database
+    this.save = function() {
+        return storage.save().then(function() {
+            return this;
+        }.bind(this));
+    }
+
+    // get and update a value
+    this.set = function(prop, val) {
+        storage[prop] = val;
+    };
+    this.get = function(prop) {
+        return storage[prop];
+    };
+
+    // set a number of properties more nicely than through get and set
+    var that = this;
+    ["origin", "destination", "amount", "executedAt"].forEach(function(property){
+        Object.defineProperty(that, property, {
+            get: function(){
+                return that.get(property);
+            },
+            set: function(value){
+                that.set(property, value)
+            }
+        });
+    });
+
+    this.toObject = function(){
+        return storage
+    };
+
+    this.toString = function(){
+        var obj = this.toObject();
+        return JSON.stringify(obj. undefined, 2);
+    }
+
+}
+
+
+module.exports = Transaction;
